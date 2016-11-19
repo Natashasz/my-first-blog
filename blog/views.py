d33b9b3e4eb1ec0cee1939e3beb75eaa90f465bf
Natasha <natasha.chan.szejer@gmail.com>
2016-11-19 17:52:02 +0100

    add last2

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('?')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_new(request):
